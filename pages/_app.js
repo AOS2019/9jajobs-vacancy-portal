3a06ac35d566c52501fe19f48fde4af0c7e13995
AOS2019 <samuel90ezekiel@gmail.com>
2025-03-25 18:29:07 +0100

    lib folder was added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../public/assets/css/styles.css"; // adjust the path if needed
+//import "../public/assets/css/styles.css"; // adjust the path if needed
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
